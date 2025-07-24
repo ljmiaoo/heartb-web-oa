384f79d73496d080933b52641606dec13c2dea4f
lilyzhou <lj_zhou@outlook.com>
2025-07-24 11:02:02 +0800

    ui: page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
 import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
 
 export const metadata: Metadata = {
   title: {
@@ -43,11 +41,9 @@ export default function RootLayout({
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            <main className="container mx-auto max-w-7xl flex-grow">
               {children}
             </main>
-            <footer className="w-full flex items-center justify-center py-3" />
           </div>
         </Providers>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-"use client";
-import { Icon } from "@iconify/react";
+'use client';
+import { Icon } from '@iconify/react';
 import {
   Button,
   Card,
@@ -10,14 +10,14 @@ import {
   ScrollShadow,
   Select,
   SelectItem,
-} from "@nextui-org/react";
-import { cn } from "@nextui-org/theme";
-import React, { useMemo, useState } from "react";
-import useSWR from "swr";
+} from '@nextui-org/react';
+import { cn } from '@nextui-org/theme';
+import React, { useMemo, useState } from 'react';
+import useSWR from 'swr';
 
 export default function Home() {
   /* ----------------------------- get novel list ----------------------------- */
-  const { data } = useSWR("/api/novel-list");
+  const { data } = useSWR('/api/novel-list');
   const novels: { key: string; label: string }[] = useMemo(() => {
     return data?.novels?.map((n: string) => ({ key: n, label: n })) ?? [];
   }, [data]);
@@ -25,14 +25,14 @@ export default function Home() {
   /* ----------------------------- select a novel ----------------------------- */
   const [novelId, setNovelId] = useState<string | undefined>();
   const { data: novelContent } = useSWR(
-    novelId ? `/api/novel/${novelId}` : null,
+    novelId ? `/api/novel/${novelId}` : null
   );
 
-  console.log(novelContent)
+  console.log(novelContent);
 
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div>
+    <section className="flex flex-col h-screen items-center">
+      <div className="py-3 px-6 border-b w-full">
         <div className="w-48">
           <Select
             items={novels}
@@ -46,173 +46,170 @@ export default function Home() {
             {(story) => <SelectItem key={story.key}>{story.label}</SelectItem>}
           </Select>
         </div>
-        <div className="pt-6 flex flex-row ">
-          <div
-            className={cn(
-              "relative flex h-full w-96 max-w-[384px] flex-1 flex-col !border-r-small border-divider pr-6 transition-[transform,opacity,margin] duration-250 ease-in-out",
-            )}
-            id="menu"
-          >
-            <header className="flex items-center text-md font-medium text-default-500 group-data-[selected=true]:text-foreground">
-              <Icon
-                className="text-default-500 mr-2"
-                icon="solar:clipboard-text-outline"
-                width={24}
-              />
-              Chapters
-            </header>
-            <ScrollShadow
-              className="max-h-[calc(500px)] -mr-4"
-              id="menu-scroll"
-            >
-              <div className="flex flex-col gap-4 py-3 pr-4">
-                <Card
-                  key="card-1"
-                  isPressable
-                  className={`max-w-[384px] border-1 border-divider/15 bg-themeBlue/20`}
-                  shadow="none"
-                >
-                  <CardHeader className="flex items-center justify-between">
-                    <div className="flex gap-1.5">
-                      <Chip
-                        className="mr-1 text-themeBlue bg-themeBlue/20"
-                        radius="sm"
-                        size="sm"
-                        variant="flat"
-                      >
-                        Editing
-                      </Chip>
-                      <p className="text-left mr-1">
-                        Chapter 1 - Chapter 1 title
-                      </p>
-                    </div>
-                  </CardHeader>
+      </div>
+      <div className="w-full px-6 flex flex-row flex-1 min-h-0">
+        <div
+          className={cn(
+            'relative pt-6 flex h-full w-96 max-w-[384px] flex-1 flex-col !border-r-small border-divider pr-6 transition-[transform,opacity,margin] duration-250 ease-in-out'
+          )}
+          id="menu"
+        >
+          <header className="flex items-center text-md font-medium text-default-500 group-data-[selected=true]:text-foreground">
+            <Icon
+              className="text-default-500 mr-2"
+              icon="solar:clipboard-text-outline"
+              width={24}
+            />
+            Chapters
+          </header>
+          <ScrollShadow className="max-h-[calc(500px)] -mr-4" id="menu-scroll">
+            <div className="flex flex-col gap-4 py-3 pr-4">
+              <Card
+                key="card-1"
+                isPressable
+                className={`max-w-[384px] border-1 border-divider/15 bg-themeBlue/20`}
+                shadow="none"
+              >
+                <CardHeader className="flex items-center justify-between">
+                  <div className="flex gap-1.5">
+                    <Chip
+                      className="mr-1 text-themeBlue bg-themeBlue/20"
+                      radius="sm"
+                      size="sm"
+                      variant="flat"
+                    >
+                      Editing
+                    </Chip>
+                    <p className="text-left mr-1">
+                      Chapter 1 - Chapter 1 title
+                    </p>
+                  </div>
+                </CardHeader>
 
-                  <Divider />
-                  <CardBody>
-                    <p className="line-clamp-2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip
+                <Divider />
+                <CardBody>
+                  <p className="line-clamp-2">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip
+                  </p>
+                </CardBody>
+              </Card>
+              <Card
+                key="card-2"
+                isPressable
+                className={`max-w-[384px] border-1 border-divider/15`}
+                shadow="none"
+              >
+                <CardHeader className="flex items-center justify-between">
+                  <div className="flex gap-1.5">
+                    <p className="text-left mr-1">
+                      Chapter 2 - Chapter 2 title
                     </p>
-                  </CardBody>
-                </Card>
-                <Card
-                  key="card-2"
-                  isPressable
-                  className={`max-w-[384px] border-1 border-divider/15`}
-                  shadow="none"
-                >
-                  <CardHeader className="flex items-center justify-between">
-                    <div className="flex gap-1.5">
-                      <p className="text-left mr-1">
-                        Chapter 2 - Chapter 2 title
-                      </p>
-                    </div>
-                  </CardHeader>
-                  <Divider />
-                  <CardBody>
-                    <p className="line-clamp-2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip
+                  </div>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                  <p className="line-clamp-2">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip
+                  </p>
+                </CardBody>
+              </Card>
+              <Card
+                key="card-3"
+                isPressable
+                className={`max-w-[384px] border-1 border-divider/15`}
+                shadow="none"
+              >
+                <CardHeader className="flex items-center justify-between">
+                  <div className="flex gap-1.5">
+                    <p className="text-left mr-1">
+                      Chapter 3 - Chapter 3 title
                     </p>
-                  </CardBody>
-                </Card>
-                <Card
-                  key="card-3"
-                  isPressable
-                  className={`max-w-[384px] border-1 border-divider/15`}
-                  shadow="none"
-                >
-                  <CardHeader className="flex items-center justify-between">
-                    <div className="flex gap-1.5">
-                      <p className="text-left mr-1">
-                        Chapter 3 - Chapter 3 title
-                      </p>
-                    </div>
-                  </CardHeader>
-                  <Divider />
-                  <CardBody>
-                    <p className="line-clamp-2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip
-                    </p>
-                  </CardBody>
-                </Card>
+                  </div>
+                </CardHeader>
+                <Divider />
+                <CardBody>
+                  <p className="line-clamp-2">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip
+                  </p>
+                </CardBody>
+              </Card>
+            </div>
+          </ScrollShadow>
+        </div>
+
+        <div className="w-full flex-1 flex-col min-w-[600px] pl-6 pt-6">
+          <div className="flex flex-col">
+            <header className="flex items-center justify-between pb-2">
+              <div className="flex items-center gap-3">
+                <Button isIconOnly size="sm" variant="light">
+                  <Icon
+                    className="hideTooltip text-default-500"
+                    height={24}
+                    icon="solar:sidebar-minimalistic-outline"
+                    width={24}
+                  />
+                </Button>
+                <h4 className="text-md">Chapter 1 - Chapter 1 title</h4>
               </div>
-            </ScrollShadow>
-          </div>
-
-          <div className="w-full flex-1 flex-col min-w-[600px] pl-4">
-            <div className="flex flex-col">
-              <header className="flex items-center justify-between pb-2">
-                <div className="flex items-center gap-3">
-                  <Button isIconOnly size="sm" variant="light">
-                    <Icon
-                      className="hideTooltip text-default-500"
-                      height={24}
-                      icon="solar:sidebar-minimalistic-outline"
-                      width={24}
-                    />
-                  </Button>
-                  <h4 className="text-md">Chapter 1 - Chapter 1 title</h4>
-                </div>
-              </header>
-              <div className="w-full flex-1 flex-col min-w-[400px]">
-                <div className={cn("flex flex-col gap-4")}>
-                  <div className="flex flex-col items-start">
-                    <div className="relative mb-5 w-full h-[400px] bg-slate-50 dark:bg-gray-800 rounded-lg">
-                      <div className="absolute inset-x-4 top-4 z-10 flex justify-between items-center">
-                        <div className="flex justify-between">
-                          <Button
-                            className="mr-2 bg-white dark:bg-gray-700"
-                            size="sm"
-                            startContent={
-                              <Icon
-                                className="text-default-500"
-                                icon="ant-design:highlight-outlined"
-                                width={24}
-                              />
-                            }
-                            variant="flat"
-                          >
-                            button-1
-                          </Button>
-                        </div>
-
+            </header>
+            <div className="w-full flex-1 flex-col min-w-[400px]">
+              <div className={cn('flex flex-col gap-4')}>
+                <div className="flex flex-col items-start">
+                  <div className="relative mb-5 w-full h-[400px] bg-slate-50 dark:bg-gray-800 rounded-lg">
+                    <div className="absolute inset-x-4 top-4 z-10 flex justify-between items-center">
+                      <div className="flex justify-between">
                         <Button
                           className="mr-2 bg-white dark:bg-gray-700"
                           size="sm"
                           startContent={
                             <Icon
                               className="text-default-500"
-                              icon="material-symbols:save-outline"
+                              icon="ant-design:highlight-outlined"
                               width={24}
                             />
                           }
                           variant="flat"
                         >
-                          button-2
+                          button-1
                         </Button>
                       </div>
-                      <div>
-                        <ScrollShadow className="editScrollShow absolute left-2 right-2 bottom-10 top-12 text-base p-3 resize-none rounded-md border-solid border-inherit bg-slate-50 dark:bg-gray-800">
-                          <div className="flex w-full h-full bg-slate-50 dark:bg-gray-200 rounded-lg p-2">
-                            {/* Adjusted to use flex display for layout */}
-                            <textarea
-                              className="flex-1 p-3 resize-none rounded-md border border-transparent bg-slate-50 dark:bg-gray-200 text-gray-900" // Use flex-1 to allow the textarea to fill available space
-                            />
-                            <div className="bg-gray-100 p-1 rounded-md self-end ml-2">
-                              {/* Added margin-left to separate from textarea, align-self to position at the bottom */}
-                            </div>
+
+                      <Button
+                        className="mr-2 bg-white dark:bg-gray-700"
+                        size="sm"
+                        startContent={
+                          <Icon
+                            className="text-default-500"
+                            icon="material-symbols:save-outline"
+                            width={24}
+                          />
+                        }
+                        variant="flat"
+                      >
+                        button-2
+                      </Button>
+                    </div>
+                    <div>
+                      <ScrollShadow className="editScrollShow absolute left-2 right-2 bottom-10 top-12 text-base p-3 resize-none rounded-md border-solid border-inherit bg-slate-50 dark:bg-gray-800">
+                        <div className="flex w-full h-full bg-slate-50 dark:bg-gray-200 rounded-lg p-2">
+                          {/* Adjusted to use flex display for layout */}
+                          <textarea
+                            className="flex-1 p-3 resize-none rounded-md border border-transparent bg-slate-50 dark:bg-gray-200 text-gray-900" // Use flex-1 to allow the textarea to fill available space
+                          />
+                          <div className="bg-gray-100 p-1 rounded-md self-end ml-2">
+                            {/* Added margin-left to separate from textarea, align-self to position at the bottom */}
                           </div>
-                        </ScrollShadow>
-                      </div>
+                        </div>
+                      </ScrollShadow>
                     </div>
                   </div>
                 </div>
